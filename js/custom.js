6464e4169f4f0c8e5cf3bdfcdc6d9c0a080a23c5
doina93 <doina.ciocirlan@gmail.com>
2020-05-12 12:38:43 +0300

    added function and options for flickity carousel

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
     $('button.toggle-icons').click(function () {
-        $('nav.custom-nav').toggleClass('nav-background');
+        $('nav.custom-nav, .custom-nav-list').toggleClass('nav-background');
+    });
+    $('.blog-carousel').flickity({
+        // options
+        cellAlign: 'left',
+        contain: true,
+        draggable: '>1',
+        wrapAround: true
     });
 });
